c6a83f1e3e88ae96a184764194cb9e7b37cb5863
TarunSharma <Tarun-sharma_xft@external.mckinsey.com>
2017-07-21 20:07:29 +0530

    fina phase of app

--- www/js/app.js
+++ www/js/app.js
@@ -53,69 +53,8 @@ angular
                     .getLanguage()
                     .then(function(data) {
                         LocalizationService.setLanguage(data);
-                    })
-                    .then(function() {
-                            //if not exist check internet connection
-                            if (true) {
-                                //fetch data
-                                if (true /*navigator.connection.type != Connection.NONE*/ ) {
 
-                                    var ref = firebase.database().ref();
-                                    var obj = $firebaseObject(ref);
-                                    // to take an action after the data loads, use the $loaded() promise
-                                    obj.$loaded().then(function() {
-                                        console.log("loaded record:", obj.$id, obj.someOtherKeyInData);
-
-                                        // To iterate the key/value pairs of the object, use angular.forEach()
-                                        LocalStorageService.setEvents(obj['events']);
-                                        //LocalStorageService.setRenaissance(data.$getRecord('renaissance'));
-                                        LocalStorageService.setContacts(obj['contacts']);
-                                        // LocalStorageService.setSponsors(data.$getRecord('sponsors'));
-                                        LocalStorageService.setMapMarkers(obj['locations']);
-                                        LocalStorageService.setData('true');
-                                    });
-
-                                } else {
-                                    $ionicPopup
-                                    .alert({ title: "Internet", content: "{{'content'| translate}}" })
-                                    .then(function(res) {
-                                        if (res)
-                                            navigator.app.exitApp();
-                                    });
-                                }
-                            }
-                        })
-                }
-                //update data
-                if (!LocalStorageService.getData()) {
-                    LocalizationService
-                    .getLanguage()
-                    .then(function(data) {
-                        LocalizationService.setLanguage(data);
-                    })
-                    .then(function() {
-                        if (true) {
-                            if (true && FirebaseService.getAuth()) {
-                                var ref = firebase.database().ref();
-
-                                var obj = $firebaseObject(ref);
-
-                                    // to take an action after the data loads, use the $loaded() promise
-                                    obj.$loaded().then(function() {
-                                        console.log("loaded record:", obj.$id, obj.someOtherKeyInData);
-
-                                        // To iterate the key/value pairs of the object, use angular.forEach()
-
-                                        LocalStorageService.setEvents(obj['events']);
-                                        //LocalStorageService.setRenaissance(data.$getRecord('renaissance'));
-                                        LocalStorageService.setContacts(obj['contacts']);
-                                        // LocalStorageService.setSponsors(data.$getRecord('sponsors'));
-                                        LocalStorageService.setMapMarkers(obj['locations']);
-                                        LocalStorageService.setData('true');
-                                    });
-                                }
-                            }
-                        })
+                    });
                 }
 
                 if (window.cordova && window.cordova.plugins.Keyboard) {
@@ -129,7 +68,7 @@ angular
                 }
 
             });
-})
+    })
 .config(function($stateProvider, $urlRouterProvider, $translateProvider, $ionicCloudProvider) {
 
     $ionicCloudProvider.init({
@@ -159,17 +98,8 @@ angular
                 templateUrl: "js/modules/login/login.html",
                 controller: "authCtrl"
             }
-        },
-        resolve: {
-                    // controller will not be loaded until $requireSignIn resolves
-                    // Auth refers to our $firebaseAuth wrapper in the factory below
-                    "currentAuth": ["FirebaseService", function(FirebaseService) {
-                        // $requireSignIn returns a promise so the resolve waits for it to complete
-                        // If the promise is rejected, it will throw a $stateChangeError (see above)
-                        return !FirebaseService.getAuth()
-                    }]
-                }
-            })
+        }
+    })
     .state('app.loginauth', {
         url: "/loginauth",
         views: {
@@ -177,17 +107,8 @@ angular
                 templateUrl: "js/modules/login/auth.html",
                 controller: "authCtrl"
             }
-        },
-        resolve: {
-                    // controller will not be loaded until $requireSignIn resolves
-                    // Auth refers to our $firebaseAuth wrapper in the factory below
-                    "currentAuth": ["FirebaseService", function(FirebaseService) {
-                        // $requireSignIn returns a promise so the resolve waits for it to complete
-                        // If the promise is rejected, it will throw a $stateChangeError (see above)
-                        return !FirebaseService.getAuth()
-                    }]
-                }
-            })
+        }
+    })
     .state('app.register', {
         url: "/register",
         views: {
@@ -223,15 +144,6 @@ angular
                 templateUrl: "js/modules/kit/kit.html",
                 controller: "kitCtrl"
             }
-        }
-    })
-    .state('app.drugs', {
-        url: "/drugs",
-        abstract: true,
-        views: {
-            'menuContent': {
-                templateUrl: "js/modules/drugs/drugs.html"
-            }
         },
         resolve: {
                     // controller will not be loaded until $requireSignIn resolves
@@ -239,10 +151,20 @@ angular
                     "currentAuth": ["FirebaseService", function(FirebaseService) {
                         // $requireSignIn returns a promise so the resolve waits for it to complete
                         // If the promise is rejected, it will throw a $stateChangeError (see above)
-                        return FirebaseService._redirectIfNotAuthenticated();
+                        return FirebaseService.getApplicationData()
                     }]
                 }
-    })
+
+            })
+    .state('app.drugs', {
+        url: "/drugs",
+        abstract: true,
+        views: {
+            'menuContent': {
+                templateUrl: "js/modules/drugs/drugs.html"
+            }
+        }
+            })
     .state('app.drugs.hospital', {
         url: "/hospital/:hospital",
         views: {
@@ -250,7 +172,16 @@ angular
                 templateUrl: "js/modules/drugs/hospital.html",
                 controller: "HospitalCtrl as ec"
             }
-        }
+        },
+                resolve: {
+                    // controller will not be loaded until $requireSignIn resolves
+                    // Auth refers to our $firebaseAuth wrapper in the factory below
+                    "currentAuth": ["FirebaseService", function(FirebaseService) {
+                        // $requireSignIn returns a promise so the resolve waits for it to complete
+                        // If the promise is rejected, it will throw a $stateChangeError (see above)
+                        return FirebaseService.getApplicationData()
+                    }]
+                }
     })
     .state('app.drugs.individual', {
         url: "/individual/:individual",
@@ -259,7 +190,16 @@ angular
                 templateUrl: "js/modules/drugs/individual.html",
                 controller: "IndividualCtrl as ec"
             }
-        }
+        },
+                resolve: {
+                    // controller will not be loaded until $requireSignIn resolves
+                    // Auth refers to our $firebaseAuth wrapper in the factory below
+                    "currentAuth": ["FirebaseService", function(FirebaseService) {
+                        // $requireSignIn returns a promise so the resolve waits for it to complete
+                        // If the promise is rejected, it will throw a $stateChangeError (see above)
+                        return FirebaseService.getApplicationData()
+                    }]
+                }
     })
     .state('app.drugs.help', {
         url: "/help/:help",
@@ -268,7 +208,16 @@ angular
                 templateUrl: "js/modules/drugs/help.html",
                 controller: "HelpCtrl as ec"
             }
-        }
+        },
+                resolve: {
+                    // controller will not be loaded until $requireSignIn resolves
+                    // Auth refers to our $firebaseAuth wrapper in the factory below
+                    "currentAuth": ["FirebaseService", function(FirebaseService) {
+                        // $requireSignIn returns a promise so the resolve waits for it to complete
+                        // If the promise is rejected, it will throw a $stateChangeError (see above)
+                        return FirebaseService.getApplicationData()
+                    }]
+                }
     })
     .state('app.map', {
         url: "/map",
@@ -284,7 +233,7 @@ angular
                     "currentAuth": ["FirebaseService", function(FirebaseService) {
                         // $requireSignIn returns a promise so the resolve waits for it to complete
                         // If the promise is rejected, it will throw a $stateChangeError (see above)
-                        return FirebaseService.getServiceRef().$requireSignIn();
+                        return !FirebaseService.getApplicationData()
                     }]
                 }
             })
@@ -335,9 +284,9 @@ angular
             menu_opiods_overdose: "Opiods-Overdose",
             menu_about_opiods_overdose: "About Opiods-Overdose",
             menu_about_ren_fest: "About renaissance",
-            menu_event: "Available Kit Holders",
+            menu_event: "Available Kits",
             menu_call911: "Call 911",
-            menu_map: "Find Overdose Kit",
+            menu_map: "Overdose Kit",
 
             menu_profile: "Profile",
             menu_contact: "Contacts",
@@ -375,7 +324,7 @@ angular
             menu_opiods_overdose: "Opiods-Overdose",
             menu_about_opiods_overdose: "About Opiods-Overdose",
             menu_event: "Available kits",
-            menu_map: "Find Overdose Kit",
+            menu_map: "Overdose Kit",
             menu_kit: "I Have Kit",
             menu_profile: "Profile",
             menu_contact: "Kontakt",
--- www/js/modules/webService/firebase-service.js
+++ www/js/modules/webService/firebase-service.js
@@ -1,53 +1,82 @@
 angular.module('firebase.services', ['firebase']).factory('FirebaseService', FirebaseService);
 
-FirebaseService.$inject = ['$q', '$firebaseAuth', '$firebaseObject','$state'];
+FirebaseService.$inject = ['$q', '$firebaseAuth', '$firebaseObject', '$state','LocalStorageService','$timeout'];
 /**
  * @name FirebaseService
  * @desc Service for communication with webservice
  */
- function FirebaseService($q, $firebaseAuth, $firebaseObject,$state) {
+function FirebaseService($q, $firebaseAuth, $firebaseObject, $state,LocalStorageService,$timeout) {
 
- 	return {
-		/**
-		 * @name getData
-		 * @desc Fetch and synchronize data from web service
-		 * @param {string} lang- language (hr,en..)
-		 * @returns {Object[]|json}
-		 */
-		 getServiceRef: function() {
-		 	return $firebaseAuth();
-		 },
-		 getAuth: function() {
-		 	var firebaseUser = $firebaseAuth().$getAuth();
-		 	if (firebaseUser) {
-		 		return true;
-		 	} else {
-		 		return false;
-		 	}
-		 },
-		 getCurrentUser: function() {
-		 	return firebase.auth().currentUser;
-		 },
-		 _skipIfAuthenticated : function() {
-		 	var defer = $q.defer();
-		 	if($firebaseAuth().$getAuth()) {
-		 		defer.reject(); /* (1) */
-		 	} else {
-		 		defer.resolve(); /* (2) */
-		 	}
-		 	return defer.promise;
-		 },
-		 _redirectIfNotAuthenticated : function() {
-		 	var defer = $q.defer();
-		 	if($firebaseAuth().$getAuth()) {
-		 		defer.resolve(); /* (3) */
-		 	} else {
-		 		$timeout(function () {
-		 			$state.go('app.login'); /* (4) */
-		 		});
-		 		defer.reject();
-		 	}
-		 	return defer.promise;
-		 }
-	   };
-	}
+    return {
+        /**
+         * @name getData
+         * @desc Fetch and synchronize data from web service
+         * @param {string} lang- language (hr,en..)
+         * @returns {Object[]|json}
+         */
+        getServiceRef: function() {
+            return $firebaseAuth();
+        },
+        getApplicationData: function() {
+            var defer = $q.defer();
+
+            if ($firebaseAuth().$getAuth()) {
+                var ref = firebase.database().ref();
+                var obj = $firebaseObject(ref);
+                // to take an action after the data loads, use the $loaded() promise
+                obj.$loaded().then(function() {
+                    defer.resolve();
+                    console.log("loaded record:", obj.$id, obj.someOtherKeyInData);
+
+                    // To iterate the key/value pairs of the object, use angular.forEach()
+                    LocalStorageService.setEvents(obj['events']);
+                    //LocalStorageService.setRenaissance(data.$getRecord('renaissance'));
+                    LocalStorageService.setContacts(obj['contacts']);
+                    // LocalStorageService.setSponsors(data.$getRecord('sponsors'));
+                    LocalStorageService.setMapMarkers(obj['locations']);
+                    LocalStorageService.setData('true');
+                }, function() {
+                    defer.reject();
+                });
+            } else {
+                $timeout(function() {
+                    $state.go('app.login'); /* (4) */
+                });
+                defer.reject();
+            }
+            return defer.promise;
+        },
+        getAuth: function() {
+            var firebaseUser = $firebaseAuth().$getAuth();
+            if (firebaseUser) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        getCurrentUser: function() {
+            return firebase.auth().currentUser;
+        },
+        _skipIfAuthenticated: function() {
+            var defer = $q.defer();
+            if ($firebaseAuth().$getAuth()) {
+                defer.reject(); /* (1) */
+            } else {
+                defer.resolve(); /* (2) */
+            }
+            return defer.promise;
+        },
+        _redirectIfNotAuthenticated: function() {
+            var defer = $q.defer();
+            if ($firebaseAuth().$getAuth()) {
+                defer.resolve(); /* (3) */
+            } else {
+                $timeout(function() {
+                    $state.go('app.login'); /* (4) */
+                });
+                defer.reject();
+            }
+            return defer.promise;
+        }
+    };
+}
